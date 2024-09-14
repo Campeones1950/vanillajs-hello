7304cc1704365db68c7ef1faac7a3ac1210a3cf3
Campeones1950 <168790271+Campeones1950@users.noreply.github.com>
2024-09-14 11:15:51 +0000

    [Se rezlizan cambioas para poder borrar codespaces]

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  /*write your code here*/
+  console.log("Este es un mensjae de control de la funsión window.onload");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function randomElement(arreglo) {
+    let arregloIndex = Math.floor(Math.random() * arreglo.length);
+    return arreglo[arregloIndex];
+  }
+
+  return (
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when)
+  );
+}
