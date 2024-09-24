0a4137e60b27de57a2880d6acf68d1be2bc330d3
Campeones1950 <168790271+Campeones1950@users.noreply.github.com>
2024-09-24 18:29:05 +0000

    LIF commit cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,54 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  let valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let cambiarCartabBtn = document.getElementById("cambiarCarta");
+  let paloArriba = document.getElementById("paloArriba");
+  let paloAbajo = document.getElementById("paloAbajo");
+  let valor = document.getElementById("valor");
+  let jugaDeNuevo;
+
+  function cambiarCarta() {
+    let palosAleat = Math.floor(Math.random() * palos.length);
+
+    let valoresAleat = Math.floor(Math.random() * valores.length);
+
+    paloArriba.innerHTML = palos[palosAleat];
+    paloAbajo.innerHTML = palos[palosAleat];
+    valor.innerHTML = valores[valoresAleat];
+
+    if (palos[palosAleat] == "♥" || palos[palosAleat] == "♦") {
+      paloArriba.style.color = "red";
+      paloAbajo.style.color = "red";
+    } else {
+      paloArriba.style.color = "black";
+      paloAbajo.style.color = "black";
+    }
+
+    if (jugaDeNuevo) {
+      clearTimeout(jugaDeNuevo);
+    }
+    jugaDeNuevo = setTimeout(function() {
+      alert("¿El juego puede ser adictivo, cuidado?");
+    }, 2000);
+  }
+
+  cambiarCartabBtn.addEventListener("click", cambiarCarta);
 };
